c7e068f2e850d7fa21d64e16634e1d192779efa2
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-09-26 21:24:52 -0500

    restructure and refactor component so that it is modular.

--- src/components/ResumeSection.jsx
+++ src/components/ResumeSection.jsx
@@ -1,36 +1,40 @@
+import ExperienceBlock from "./ExperienceBlock"
 
 function ResumeSection(props) {
 
+    let singleEntry;
+    let multipleEntries;
+    let sectionHeader;
+
     if (props.sType === "education") {
-        if (props.information.education.length === 0) {
-            return (
-                <div></div>
-            )
-        } else {
-            return (
-                <div className={props.sType}>
-                    {props.information.education.map((entry) => {
-                        return <p key={entry.school}> {entry.school} {entry.major} {entry.start}-{entry.end} {entry.location}</p>
-                    })}
-                </div>
-            )
-        }
+        singleEntry = props.allInfo.eInfo1;
+        multipleEntries = props.allInfo.eInfo2;
+        sectionHeader = "Education";
 
-    } else if (props.sType === "work") {
-        if (props.information.work.length === 0) {
-            return (
-                <div></div>
-            )
-        } else {
-            return (
-                <div className={props.sType}>
-                    {props.information.work.map((entry) => {
-                        return <p key={entry.company}> {entry.company} {entry.role} {entry.start}-{entry.end}</p>
-                    })}
-                </div>
-            )
-        }
+    } else {
+        singleEntry = props.allInfo.wInfo1;
+        multipleEntries = props.allInfo.wInfo2;
+        sectionHeader = "Work Experience";
 
+    }
+
+    if (multipleEntries.length === 0) {
+        return (
+            <ExperienceBlock blockInfo={singleEntry}></ExperienceBlock>
+        )
+
+    } else {
+        return (
+            <>
+                <h2 style={{ marginBottom: 20, padding: 5, backgroundColor: "grey", borderRadius: 3}}>{sectionHeader}</h2>
+                <div className={props.sType}>
+                    {multipleEntries.map((entry) => {
+                        return <ExperienceBlock key={entry.institution} blockInfo={entry}></ExperienceBlock>
+                    })}
+                    <ExperienceBlock blockInfo={singleEntry}></ExperienceBlock>
+                </div>
+            </>
+        )
     }
 
 }
