ed6163a627c315bf1f08235b7cc540d5579de80d
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-09-26 21:30:24 -0500

    refactor code to make it more modular. Include three conditional checks for different states. Add CSS to inputs.

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -1,24 +1,77 @@
+import "./CustomInput.css"
 
+function CustomInput({taStat, editEntry, allInfo, allChangers, identifier, labelText, sectionType, propertyName}) {
 
-function CustomInput({information, identifier, labelText, changer, propertyName}) {
-
+    let properChanger;
+    if (!sectionType) properChanger = allChangers.pChanger;
+    if (sectionType === "edForm") properChanger = allChangers.eChanger1;
+    if (sectionType === "workForm") properChanger = allChangers.wChanger1;
     const handleChange = (e) => {
-        changer(prevState => ({...prevState, [propertyName]: e.target.value }));
+        properChanger(prevState => ({...prevState, [propertyName]: e.target.value }));
     }
 
-    return (
-        <label htmlFor={identifier}>
-            {labelText}: 
-            <input 
-                type="text" 
-                id={identifier} 
-                name={identifier}
-                onChange={handleChange}
-                value={information[propertyName]}
-            />
-        </label>
-    )    
+    if (!sectionType) {
 
+        return (
+            <div className="inputs">
+                <label htmlFor={identifier}>{labelText}: </label>
+                <input 
+                    type="text" 
+                    id={identifier} 
+                    name={identifier}
+                    onChange={handleChange}
+                    value={allInfo.pInfo[propertyName]}
+                />
+            </div>
+        )
+
+    } else if (sectionType && editEntry !== undefined && Object.keys(editEntry).length > 0) {
+
+        return (
+            <div className="inputs">
+                <label htmlFor={identifier}>{labelText}: </label>
+                {(sectionType === "workForm" && taStat === "true") ? 
+                    <textarea
+                        id={identifier} 
+                        name={identifier}
+                        onChange={handleChange}
+                        value={editEntry[propertyName]}
+                    >
+                    </textarea> :
+                    <input 
+                        type="text" 
+                        id={identifier} 
+                        name={identifier}
+                        onChange={handleChange}
+                        value={editEntry[propertyName]}
+                    />
+                }
+            </div>
+        )
+
+    } else {
+
+        return (
+            <div className="inputs">
+                <label htmlFor={identifier}>{labelText}: </label>
+                {(sectionType === "workForm" && taStat === "true") ?
+                    <textarea
+                        id={identifier} 
+                        name={identifier}
+                        onChange={handleChange}
+                    >
+                    </textarea> :
+                    <input 
+                        type="text" 
+                        id={identifier} 
+                        name={identifier}
+                        onChange={handleChange}
+                    />
+                }
+            </div>
+        )
+
+    }
 
 }
 
