0b22b466c1095c8632ccdb711f4717f8c94246a5
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-09-26 21:22:48 -0500

    restructure Resume component so that it has a personal info section at the top followed by two ResumeSection components that log ed/work experience. This also adds the CSS styling for the resume.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,10 +6,17 @@ function Resume(props) {
     return (
         <div className="resume">
             <div className="personal">
-                <p>{props.information.name} {props.information.email} {props.information.phone} {props.information.address}</p>
+                <div style={{fontSize: 24}}><b>{props.allInfo.pInfo.name}</b></div>
+                <div className="personalBottom">
+                    <p>{props.allInfo.pInfo.email}</p>
+                    <p>{props.allInfo.pInfo.phone}</p>
+                    <p>{props.allInfo.pInfo.address}</p>
+                </div>
             </div>
-            <ResumeSection information={props.information} sType="education"></ResumeSection>
-            <ResumeSection information={props.information} sType="work"></ResumeSection>
+            <div className="nonPersonal">
+                <ResumeSection allInfo={props.allInfo} sType="education"></ResumeSection>
+                <ResumeSection allInfo={props.allInfo} sType="work"></ResumeSection>
+            </div>
         </div>
     )
 
