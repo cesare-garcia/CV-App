cb34fc0fcbe82a82124ce7ab4e37cf790e88e31d
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-09-27 11:50:40 -0500

    add additional starting objects for testing purposes.

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,75 @@ import Form from './components/Form';
 function App() {
   
   const [personal, setPersonal] = useState({
-    name: "Cesar Garcia",
-    email: "cegarcia003@gmail.com",
-    phone: "859 806 6713",
-    address: "2825 Main Street Buda, Texas 78610",
+    name: "Luke Dunphy",
+    email: "ldunphy@gmail.com",
+    phone: "980 748 2342",
+    address: "Los Olives Avenue, Los Angeles, 84938",
   });
 
   const [edEntry, setEdEntry] = useState({});
 
   const [edCollection, setEdCollection] = useState([{
-    institution: "Centre College",
-    focus: "IST",
-    start: 2014,
-    end: 2018,
-    location: "Danville, KY"
-  }]);
+    institution: "University of Oregon",
+    focus: "Real Estate",
+    start: "2020",
+    end: "2024",
+    location: "Portland, OR"
+  },
+  {
+    institution: "Udemy",
+    focus: "SQL",
+    start: "2020",
+    end: "2024",
+    location: "Portland, OR"
+  },
+  {
+    institution: "Coursera",
+    focus: "React",
+    start: "2020",
+    end: "2024",
+    location: "Portland, OR"
+  }
+]);
 
   const [workEntry, setWorkEntry] = useState({});
 
   const [workCollection, setWorkCollection] = useState([{
-    institution: "Epic Systems",
-    focus: "Project Manager",
-    start: 2018,
-    end: 2019,
-    location: "Verona, WI",
-    description: "Served as the Application Coordinator for the Cleveland Clinic Implementation of Willow Ambulatory."
-  }])
+    institution: "Los Angeles Golf Club",
+    focus: "Waiter",
+    start: "2018",
+    end: "2020",
+    location: "Los Angeles, CA",
+    description: "Served as the head waiter for the golf club."
+  },
+  {
+    institution: "Dunphy Magic Store",
+    focus: "Assistant Manager",
+    start: "2017",
+    end: "2018",
+    location: "Los Angeles, CA",
+    description: "Assisted."
+  },
+  {
+    institution: "Antarctic Labor",
+    focus: "Driver",
+    start: "2016",
+    end: "2017",
+    location: "Antarctica",
+    description: "Drove."
+  }
+])
 
   const clearAll = (e) => {
     e.preventDefault();
+    let name = document.querySelector("#my-name");
+    name.value = "";
+    let email = document.querySelector("#my-email");
+    email.value = "";
+    let phone = document.querySelector("#my-phone");
+    phone.value = "";
+    let address = document.querySelector("#my-address");
+    address.value = "";
     setPersonal({});
     setEdEntry({});
     setEdCollection([]);
