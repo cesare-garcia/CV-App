a6b0107aa4706a206fd1b9a5ee9251d4f87bf504
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-09-26 21:20:14 -0500

    restructure the app file so that personal information is tracked separately from ed and work info. This will also split individual ed/work entries from the arrays that hold the collections

--- src/App.jsx
+++ src/App.jsx
@@ -5,53 +5,65 @@ import Form from './components/Form';
 
 function App() {
   
-  const [resume, setResume] = useState({
+  const [personal, setPersonal] = useState({
     name: "Cesar Garcia",
     email: "cegarcia003@gmail.com",
     phone: "859 806 6713",
     address: "2825 Main Street Buda, Texas 78610",
-    education: [{
-      school: "Centre",
-      major: "IST",
-      start: "2014",
-      end: "2018",
-      location: "Danville"
-    }],
-    work: [{
-      company: "Epic",
-      role: "Project Manager",
-      start: "2018",
-      end: "2019"
-    }]
   });
+
+  const [edEntry, setEdEntry] = useState({});
+
+  const [edCollection, setEdCollection] = useState([{
+    institution: "Centre College",
+    focus: "IST",
+    start: 2014,
+    end: 2018,
+    location: "Danville, KY"
+  }]);
+
+  const [workEntry, setWorkEntry] = useState({});
+
+  const [workCollection, setWorkCollection] = useState([{
+    institution: "Epic Systems",
+    focus: "Project Manager",
+    start: 2018,
+    end: 2019,
+    location: "Verona, WI",
+    description: "Served as the Application Coordinator for the Cleveland Clinic Implementation of Willow Ambulatory."
+  }])
 
   const clearAll = (e) => {
     e.preventDefault();
-    setResume({
-      name: "",
-      email: "",
-      phone: "",
-      address: "",
-      education: [{
-        school: "",
-        major: "",
-        start: "",
-        end: "",
-        location: ""
-      }],
-      work: [{
-        company: "",
-        role: "",
-        start: "",
-        end: ""
-      }]
-    })
+    setPersonal({});
+    setEdEntry({});
+    setEdCollection([]);
+    setWorkEntry({});
+    setWorkCollection([])
   }
+
+  const changerObject = {
+    pChanger: setPersonal,
+    eChanger1: setEdEntry,
+    eChanger2: setEdCollection,
+    wChanger1: setWorkEntry,
+    wChanger2: setWorkCollection,
+    reset: clearAll
+  }
+
+  const infoObject = {
+    pInfo: personal,
+    eInfo1: edEntry,
+    eInfo2: edCollection,
+    wInfo1: workEntry,
+    wInfo2: workCollection
+  }
+
 
   return (
     <div className="appContainer">
-      <Form information={resume} changer={setResume} clear={clearAll}></Form>
-      <Resume information={resume}></Resume>
+      <Form allInfo={infoObject} allChangers={changerObject} ></Form>
+      <Resume allInfo={infoObject} ></Resume>
     </div>
 
   )
